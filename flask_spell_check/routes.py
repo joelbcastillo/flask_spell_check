b5abbc60f7d37a5dbc0a37d3e7e6af3b7f88d6b1
Joel Castillo <jocastillo@records.nyc.gov>
2019-10-27 20:05:22 -0400

    Fix ZAP Issues:
    Added frame-ancestors: 'none' to CSP policy as this does not fall back to
    default-src directive.

--- flask_spell_check/routes.py
+++ flask_spell_check/routes.py
@@ -26,7 +26,7 @@ def login_required(view):
 
 @bp.after_request
 def add_security_headers(response):
-    response.headers['Content-Security-Policy'] = "default-src 'self'"
+    response.headers['Content-Security-Policy'] = "default-src 'self'; frame-ancestors 'none';"
     response.headers['X-Content-Type-Options'] = 'nosniff'
     response.headers['X-Frame-Options'] = 'SAMEORIGIN'
     response.headers['X-XSS-Protection'] = '1; mode=block'
